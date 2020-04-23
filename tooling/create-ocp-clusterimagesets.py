4bb6aa252dc0d8eea392e9e9c07a841a796d7a51
Joshua Packer <jpacker@redhat.com>
2020-04-23 17:42:44 -0400

    Major update
    - Now keep 2 latest stable stream plus 4.3.0
    - Keep 2 latest 4.4rc's
    - Add a script for automatic processing.

---
+++ tooling/create-ocp-clusterimagesets.py
@@ -0,0 +1,29 @@
+# Author: jnpacker
+
+import requests
+import os.path
+
+# To support new versions, add them to this array
+for version in ["4.3", "4.4"]:
+    resp = requests.get('https://quay.io/api/v1/repository/openshift-release-dev/ocp-release/image/')
+    if resp.status_code != 200:
+        # There was a problem
+        raise ApiError('GET /api/v1/ {}'.format(resp.status_code))
+    # Loop through all images found in quay.io
+    for image in resp.json()['images']:
+        if (image['tags'] != []):
+            tag=image['tags'][0]  # There is only one tag for now in each image
+            if ("x86_64" in tag and version in tag):
+                print('Checking tag: {}'.format(tag), end='')
+                # Check if we already have the file in the primary or archive
+                fileName="clusterImageSets/" + version + "/img" + tag + ".yaml"
+                if not (os.path.isfile(fileName) or os.path.isfile("clusterImageSets/archive/" + version + "/img" + tag + ".yaml")):
+                    imgName=tag.replace("_","-")
+                    yaml= open(fileName,"w+")
+                    yaml.write("---\napiVersion: hive.openshift.io/v1\nkind: ClusterImageSet\nmetadata:\n    name: img" + imgName + "\nspec:\n    releaseImage: quay.io/openshift-release-dev/ocp-release:" + tag + "\n")
+                    yaml.close()
+                    print(" Created clusterImageSet")
+                else:
+                    print(" Skipped, already exists")
+
+# Add support to pull from these YAML: https://github.com/openshift/cincinnati-graph-data/blob/master/channels/stable-4.3.yaml
