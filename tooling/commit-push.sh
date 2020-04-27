ae17041e2b96986ca02baaf8718a55b617fe3adf
Joshua Packer <jpacker@redhat.com>
2020-04-27 12:01:09 -0400

    BOT Test

--- tooling/commit-push.sh
+++ tooling/commit-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git checkout -b $GH_BRANCH
-git add ./cluster_imageSets *
+git add ./clusterImageSets *
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 git remote add acm-hive-ocp-releases https://${GH_TOKEN}@github.com/open-cluster-management/acm-hive-openshift-releases.git > /dev/null 2>&1
