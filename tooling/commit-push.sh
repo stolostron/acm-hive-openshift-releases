12e9bd99fbbf4154165881a9a948ea8c1698351a
BOT-RHACM Hive Open Shift releases <jpacker@redhat.com>
2020-04-27 12:23:26 -0400

    Travis build:

--- tooling/commit-push.sh
+++ tooling/commit-push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-git checkout -b $GH_BRANCH
-git add ./clusterImageSets *
+#git checkout -b $GH_BRANCH
+git status
+git add .
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 git remote add acm-hive-ocp-releases https://${GH_TOKEN}@github.com/open-cluster-management/acm-hive-openshift-releases.git > /dev/null 2>&1
-git push --quiet --set-upstream acm-hive-ocp-releases $GH_BRANCH
+git push --set-upstream acm-hive-ocp-releases $GH_BRANCH
--- tooling/setup-env.sh
+++ tooling/setup-env.sh
@@ -4,4 +4,4 @@ echo "Configure global user information:"
 git config --global user.email "jpacker@redhat.com"
 git config --global user.name "BOT-RHACM Hive Open Shift releases"
 
-python3 -m pip install requests
+#python3 -m pip install requests
