c1c50e0a2008c05220fd6bba4975c702175615c2
Joshua Packer <jpacker@redhat.com>
2020-04-27 11:31:30 -0400

    Inlcude pip request module

--- tooling/setup-env.sh
+++ tooling/setup-env.sh
@@ -3,3 +3,5 @@ echo "Configure global user information:"
 
 git config --global user.email "jpacker@redhat.com"
 git config --global user.name "BOT-RHACM Hive Open Shift releases"
+
+pip install request
