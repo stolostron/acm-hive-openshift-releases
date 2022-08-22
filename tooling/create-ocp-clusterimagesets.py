b8d640b038bace2e9d645bdd13bb891c7bc57a28
DangPeng Liu <daliu@redhat.com>
2022-08-22 11:20:44 +0800

    fix visible (#49)
    Signed-off-by: ldpliu <daliu@redhat.com>
    
    Signed-off-by: ldpliu <daliu@redhat.com>

--- tooling/create-ocp-clusterimagesets.py
+++ tooling/create-ocp-clusterimagesets.py
@@ -17,36 +17,38 @@ for version in VERSIONS:
         print(" Create directory: " + newDir)
         os.mkdir(newDir)
     
-    resp = requests.get('https://quay.io/api/v1/repository/openshift-release-dev/ocp-release')
-    if resp.status_code != 200:
-        # There was a problem
-        raise ValueError('GET quay.io status code: %s\n%s' % (resp.status_code, resp.text))
-    # Loop through all images found in quay.io
-    for tag in resp.json()['tags']:
-        if ("x86_64" in tag and version+"." in tag):
-            print('Checking tag: {}'.format(tag), end='')
-            # Check if we already have the file in the stable, fast or releases channel
-            fileName="img" + tag + ".yaml"
-            fileNotFound=True
-            #for channel in ["releases","stable","fast"]:
-            #    if os.path.isfile("clusterImageSets/" + channel + "/" + version + "/" + fileName):
-            #        fileNotFound=False
-            #if fileNotFound:
-            if not os.path.isfile("clusterImageSets/releases/" + version + "/" + fileName):
-                #imgName=tag.replace("x86_64","fast")
-                imgName=tag.replace("_","-")
-                yaml= open("clusterImageSets/releases/" + version + "/" + fileName,"w+")
-                cisr = ("---\n" +
-                        "apiVersion: hive.openshift.io/v1\n"
-                        "kind: ClusterImageSet\nmetadata:\n"
-                        "  name: img" + imgName + "-appsub\n"
-                        "  labels:\n"
-                        "    channel: candidate\n"
-                        "    visible: \"false\"\n"
-                        "spec:\n"
-                        "  releaseImage: quay.io/openshift-release-dev/ocp-release:" + tag + "\n")
-                yaml.write(cisr)
-                yaml.close()
-                print(" Created clusterImageSet")
-            else:
-                print(" Skipped, already exists")
+    i = 1
+    while True:
+        resp = requests.get('https://quay.io/api/v1/repository/openshift-release-dev/ocp-release/tag', "page="+str(i))
+        if resp.status_code != 200:
+            # There was a problem
+            raise ValueError('GET quay.io status code: %s\n%s' % (resp.status_code, resp.text))
+
+        # Loop through all images found in quay.io
+        for tagInfo in resp.json()['tags']:
+            tag = tagInfo["name"]
+            if ("x86_64" in tag and version+"." in tag):
+                print('Checking tag: {}'.format(tag), end='')
+                # Check if we already have the file in the stable, fast or releases channel
+                fileName="img" + tag + ".yaml"
+                fileNotFound=True
+                if not os.path.isfile("clusterImageSets/releases/" + version + "/" + fileName):
+                    imgName=tag.replace("_","-")
+                    yaml= open("clusterImageSets/releases/" + version + "/" + fileName,"w+")
+                    cisr = ("---\n" +
+                            "apiVersion: hive.openshift.io/v1\n"
+                            "kind: ClusterImageSet\nmetadata:\n"
+                            "  name: img" + imgName + "-appsub\n"
+                            "  labels:\n"
+                            "    channel: candidate\n"
+                            "    visible: \"false\"\n"
+                            "spec:\n"
+                            "  releaseImage: quay.io/openshift-release-dev/ocp-release:" + tag + "\n")
+                    yaml.write(cisr)
+                    yaml.close()
+                    print(" Created clusterImageSet")
+                else:
+                    print(" Skipped, already exists")
+        if resp.json()['has_additional'] != True:
+            break
+        i = i+1
